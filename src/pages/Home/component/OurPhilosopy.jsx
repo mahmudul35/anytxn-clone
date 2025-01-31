b51b8b27a57ba46a57af0fe357835479e067b784
mahmudul35 <101060541+mahmudul35@users.noreply.github.com>
2025-02-01 01:08:59 +0600

    update our philosopy section

--- src/pages/Home/component/OurPhilosopy.jsx
+++ src/pages/Home/component/OurPhilosopy.jsx
@@ -82,6 +82,95 @@ const OurPhilosopy = () => {
           </div>
         </div>
       </div>
+
+      <div className="flex justify-center items-center gap-8 mt-8">
+        {/* card1 */}
+        <div className="bg-blue-50 rounded-lg shadow-lg p-6 w-[360px] h-[295px] flex flex-col space-y-3 justify-center">
+          <div className=" bg-blue-100 rounded-full w-12 h-12 flex items-center justify-center ">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="lucide lucide-share-2"
+            >
+              <circle cx="18" cy="5" r="3" />
+              <circle cx="6" cy="12" r="3" />
+              <circle cx="18" cy="19" r="3" />
+              <line x1="8.59" x2="15.42" y1="13.51" y2="17.49" />
+              <line x1="15.41" x2="8.59" y1="6.51" y2="10.49" />
+            </svg>
+          </div>
+
+          <h1 className="text-2xl font-bold mt-2">Full-suite solutions</h1>
+          <p className=" text-gray-600 mt-4">
+            Experience the ease of integration across various banking and
+            payment functions with our comprehensive suite of solutions.
+          </p>
+        </div>
+
+        {/* card2 */}
+        <div className="bg-blue-50 rounded-lg shadow-lg p-6 w-[360px] h-[295px] flex flex-col space-y-3 justify-center">
+          <div className=" bg-blue-200 rounded-full w-12 h-12 flex items-center justify-center ">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="lucide lucide-lightbulb"
+            >
+              <path d="M15 14c.2-1 .7-1.7 1.5-2.5 1-.9 1.5-2.2 1.5-3.5A6 6 0 0 0 6 8c0 1 .2 2.2 1.5 3.5.7.7 1.3 1.5 1.5 2.5" />
+              <path d="M9 18h6" />
+              <path d="M10 22h4" />
+            </svg>
+          </div>
+
+          <h1 className="text-2xl font-bold mt-2">Simplify the complex</h1>
+          <p className=" text-gray-600 mt-4">
+            Simplify complex processes and optimise your financial operations by
+            leveraging the power of AI, Blockchain, Cloud Computing, and Big
+            Data.
+          </p>
+        </div>
+
+        {/* card 3 */}
+        <div className="bg-blue-50 rounded-lg shadow-lg p-6 w-[360px]  h-[295px] flex flex-col space-y-3 justify-center">
+          <div className=" bg-blue-200 rounded-full w-12 h-12 flex items-center justify-center ">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="lucide lucide-lightbulb"
+            >
+              <path d="M15 14c.2-1 .7-1.7 1.5-2.5 1-.9 1.5-2.2 1.5-3.5A6 6 0 0 0 6 8c0 1 .2 2.2 1.5 3.5.7.7 1.3 1.5 1.5 2.5" />
+              <path d="M9 18h6" />
+              <path d="M10 22h4" />
+            </svg>
+          </div>
+
+          <h1 className="text-2xl font-bold mt-2">Cutting-edge tech</h1>
+          <p className=" text-gray-600 mt-4">
+            We seamlessly combine cutting-edge technologies, resulting in an
+            unparalleled fintech experience for financial institutions.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
